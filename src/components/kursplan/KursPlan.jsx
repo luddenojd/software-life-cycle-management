f2757515d53499a5411191f3dbebfd08516efdf2
Ludwig Nojd <ludwig.nojd@hotmail.se>
2022-10-24 14:52:52 +0200

    Kursplan component done

--- src/components/kursplan/KursPlan.jsx
+++ src/components/kursplan/KursPlan.jsx
@@ -1,7 +1,30 @@
 import React from "react"
-
-const KursPlan = () => {
-  return <div></div>
+import "./KursPlan.css"
+const KursPlan = ({ updatePage }) => {
+  return (
+    <div className="wrapper">
+      <div className="form-start">
+        <h2>
+          Hur relevant tycker du att kursplanen har varit för din utbildning?
+        </h2>
+        <div className="radio-buttons">
+          <input type="radio" name="choice" />
+          <p>Helt orelevant</p>
+          <input type="radio" name="choice" />
+          <p>Ganska orelevant</p>
+          <input type="radio" name="choice" />
+          <p>Relevant</p>
+          <input type="radio" name="choice" />
+          <p>Väldigt relevant</p>
+          <input type="radio" name="choice" />
+          <p>Oerhört relevant</p>
+        </div>
+        <button onClick={() => updatePage(null)}>
+          <h4>Nästa</h4>
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default KursPlan
